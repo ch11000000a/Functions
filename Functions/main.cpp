25e3708ade09110c62a523118a6e101222ebf7db
ch11000000a <1100000011010111@mail.ru>
2022-11-07 21:17:38 +0700

    add Power

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -1,31 +1,34 @@
 ﻿#include<iostream>
 using namespace std;
-int Sum(int a=0, int b=0);// прототип функции function declaration 
+int Sum(int a = 0, int b = 0);// прототип функции function declaration 
 int Difference(int a, int b);
 int Product(int a, int b);
 double Quotient(int a, int b);
+int Power(int a, int b) {
+	int save=1;
+		for (int i = 1; i <= b; i++)
+		{
+			save *= a;
+		}
+	return save;
+}
 void main()
 {
 	setlocale(LC_ALL, "");
 	int a, b;
-	char z;
 	cout << "Введите значения: ";
-	cin >> a>>z>> b;
+	cin >> a >> b;
 	int c = Sum(a, b);// Вызов функции Function 
 	// too few arguments in function call
 	// too many arguments in function call
 	//Function dosn't take N arguments
-	if (z == '+') {
-		cout << "\n Сумма: " << Sum(a, b);
-	}if (z == '-') {
-		cout << "\n Разница: " << Difference(a, b);
-	}if (z == '*') {
-		cout << "\n Умножение: " << Product(a, b);
-	}if (z == '/') {
-		cout << "\n Деление: " << Quotient(a, b);
-	}
+	cout << "\n Сумма: " << a << "+" << b << " = " << Sum(a, b);
+	cout << "\n Разница: " << a << "-" << b << " = " << Difference(a, b);
+	cout << "\n Умножение: " << a << "*" << b << " = " << Product(a, b);
+	cout << "\n Деление: " << a << "/" << b << " = " << Quotient(a, b);
+	cout << "\n Возведение в степень " << a << "^" << b << " = " << Power(a,b);
 }
-int Sum(int a,int b) // Реализация функции определение функции function definition 
+int Sum(int a, int b) // Реализация функции определение функции function definition 
 {
 	int sum = a + b;
 	return sum;
