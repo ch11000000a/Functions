236ffcb8f6225fd0a9408f1c512681e384220030
ch11000000a <1100000011010111@mail.ru>
2022-11-13 22:32:04 +0700

    update

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 void FillRand(int arr[], const int n);
+void FillRand(double arr[], const int n);
 void Print(int arr[], const int n);
+void Print(double arr[], const int n);
 void Sort(int arr[], const int n);
+void Sort(double arr[], const int n);
+int Sum(int arr[], const int n);
+double Sum(double arr[], const int n);
+double AVG(int arr[], const int n);
+double AVG(double arr[], const int n);
+int maxValueIn(int arr[], const int n);
+double maxValueIn(double arr[], const int n);
+int minValueIn(int arr[], const int n);
+double minValueIn(double arr[], const int n);
+ 
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -13,12 +25,91 @@ void main()
 	Print(arr, n);
 	Sort(arr, n);
 	Print(arr, n);
+	cout << "Сумма масcива: " << Sum(arr, n) << endl;
+	cout << "Среднее арифметическое значение: " << AVG(arr, n) << endl;
+	cout << "Минимальное значение в масиве " << minValueIn(arr,n)<< endl;
+	cout << "Максимальное значение в масиве " << maxValueIn(arr,n)<< endl;
+
+	const int SIZE = 8;
+	double brr[SIZE];
+	FillRand(brr, SIZE);
+	Print(brr, SIZE);
+	Sort(brr, SIZE);
+	Print(brr, SIZE);
+	cout << "Сумма масcива: " << Sum(brr, SIZE) << endl;
+	cout << "Среднее арифметическое значение: " << AVG(brr, SIZE) << endl;
+	cout << "Минимальное значение в масиве " << minValueIn(brr, SIZE) << endl;
+	cout << "Максимальное значение в масиве " << maxValueIn(brr, SIZE) << endl;
+
+}
+int minValueIn(int arr[], const int n) {
+	int min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)
+			min=arr[i];
+	}
+	return min;
+}double minValueIn(double arr[], const int n) {
+	double min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)
+			min=arr[i];
+	}
+	return min;
+}
+int maxValueIn(int arr[], const int n) {
+	int max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)
+			max=arr[i];
+	}
+	return max;
+}double maxValueIn(double arr[], const int n) {
+	double max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)
+			max=arr[i];
+	}
+	return max;
+}
+double AVG(double arr[], const int n) {
+	return Sum(arr, n) / n;
+}double AVG(int arr[], const int n) {
+	return (double)Sum(arr, n) / n;
+}
+
+int Sum(int arr[], const int n) {
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}double Sum(double arr[], const int n) {
+	double sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
 }
 void FillRand(int arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
 		arr[i] = rand() % 100;
+	}
+}
+void FillRand(double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 10000;
+		arr[i] /= 100;
 	}
 }
 
@@ -29,7 +120,15 @@ void Print(int arr[], const int n)
 		cout << arr[i] << "\t";
 	}
 	cout << endl;
+}void Print(double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
 }
+
 void Sort(int arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
@@ -38,6 +137,18 @@ void Sort(int arr[], const int n)
 			if (arr[j] < arr[i])
 			{
 				int buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+	}
+}void Sort(double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+			if (arr[j] < arr[i])
+			{
+				double buffer = arr[i];
 				arr[i] = arr[j];
 				arr[j] = buffer;
 			}
